9daa373a76f006dbd5610f5cf75c1d5709fe8a3b
wenglerc <carolin.wnglr@gmail.com>
2022-10-16 18:28:35 +0200

    Fix: doppelter pwert für zweiseitigen test

--- R/confset.R
+++ R/confset.R
@@ -94,7 +94,7 @@ ConfSet <- function(data,
                         pvalue <- tGKF(sortedUnions[[i]],
                                        data[S %in% sortedUnions[[i]],],
                                        threshold = teststatistics[i])
-                        if (testcase == 2) pvalue <- pvalue * 2
+                        if (testcase == 1) pvalue <- pvalue * 2
                         # Akzeptiere V_k, wenn p(x_k, V_k) >= alpha
                         if (pvalue < alpha) {
                             accept.index <- ifelse(i == N, 0, i + 1)
@@ -109,7 +109,7 @@ ConfSet <- function(data,
                         pvalue <-
                             length(mboot.max[mboot.max >= teststatistics[i]]) /
                             length(mboot.max)
-                        if (testcase == 2) pvalue <- pvalue * 2
+                        if (testcase == 1) pvalue <- pvalue * 2
                         # Akzeptiere V_k, wenn p(x_k, V_k) >= alpha
                         if (pvalue < alpha) {
                             accept.index <- ifelse(i == N, 0, i + 1)
